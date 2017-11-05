7c2bf6516a91a0d115a2eefa5c568ce3ef104437
ChavaO777 <salvador@orozco.in>
2017-11-04 21:29:37 -0600

    changed 3.1415... to the constant M_PI, added functions to convert from degrees to radians and viceversa, a function to display the input, and changed a few variable names for better names in terms of precision and ortography

--- src/FinalProject.c
+++ src/FinalProject.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <math.h>
-#define radius 6371 // authalic radius based on/extracted from surface area;
-#define radianes (3.1415926536 / 180) //approximation of the radius of the average circumference
+#define EARTH_RADIUS_KM 6371 // authalic radius based on/extracted from surface area;
+#define radianes (M_PI / 180.0) //approximation of the radius of the average circumference
+
 /** Structure of an individual. It includes an array of integers representing
 *   the permutation of the n cities (0 to n - 1) that form that solution and 
 *   the total distance traveled to visit all n cities in that order and come
@@ -20,39 +21,70 @@ struct individual {
 };//end of the individual struct
 
 //Function that reads the input
-void readInput(struct city citys[], int numberCities){
-  //  int length = sizeof(citys)/sizeof(struct);
+void readInput(struct city citiesArray[], int numberCities){
+  
     for(int i = 0; i < numberCities; i++){
-        citys[i].id = i+1;
-        scanf("%lf %lf", &citys[i].latitude, &citys[i].longitude);
+        
+        citiesArray[i].id = i + 1;
+        scanf("%lf %lf", &citiesArray[i].latitude, &citiesArray[i].longitude);
     }
 }//end of the readInput function 
-//fnction that reads the number of cities
-int readCitys(){
+
+//function that reads the number of cities
+int readCities(){
+    
     int totalCities;
     scanf("%d", &totalCities);
     return totalCities;
 }
+
+void displayCities(struct city citiesArray[], int numberCities){
+
+    printf("\n");
+
+    for(int i = 0; i < numberCities; i++){
+
+        printf("City id: %d; Latitude: %lf; Longitude: %f\n", citiesArray[i].id, citiesArray[i].latitude, citiesArray[i].longitude);
+    }
+
+    printf("\n");
+}
+
+// This function converts decimal degrees to radians
+double convertDegreesToRadians(double degrees) {
+    
+    return (degrees * M_PI / 180);
+}
+  
+// This function converts radians to decimal degrees
+double convertRadiansToDegrees(double radians) {
+    
+    return (radians * 180.0 / M_PI);
+}
+
 /**
 *   Computes the distance between two cities given their indices. It is an implementation
 *   of the Haversine formula (https://en.wikipedia.org/wiki/Haversine_formula)
 */
-double computeDistance(int indexCoord1, int indexCoord2, struct city cities[]){
+double computeDistance(int indexCoord1, int indexCoord2, struct city citiesArray[]){
 
-    double part1, part2, part3, longitude1,longitude2 ,latitude1,latitude2;
-    longitude1 = cities[indexCoord1].longitude; 
-    longitude2 = cities[indexCoord2].longitude;
-    latitude1 = cities[indexCoord1].latitude;
-    latitude2 = cities[indexCoord2].latitude;
-    longitude1 *= radianes;//transform to radians
-    longitude2 *= radianes;//transform to radians
-    latitude1*= radianes; //transform to radians
-    latitude2 *= radianes;//transform to radians
-    double latitude = latitude2 -latitude1;//diferential betwen latitudes
-    double longitude = longitude2 -longitude1;//diferential betwen longitudes
+    double lat1deg, lng1deg, lat2deg, lng2deg; 
+    
+    lat1deg = citiesArray[indexCoord1].latitude; //Read the latitude of the first city in degrees
+    lng1deg = citiesArray[indexCoord1].longitude; //Read the longitude of the first city in degrees    
+    lat2deg = citiesArray[indexCoord2].latitude; //Read the latitude of the second city in degrees
+    lng2deg = citiesArray[indexCoord2].longitude; //Read the longitude of the second city in degrees
+
+    double lat1rad, lng1rad, lat2rad, lng2rad, u, v;
+    lat1rad = convertDegreesToRadians(lat1deg); //Convert the latitude of the first city to degrees
+    lng1rad = convertDegreesToRadians(lng1deg); //Convert the longitude of the first city to degrees
+    lat2rad = convertDegreesToRadians(lat2deg); //Convert the latitude of the second city to degrees
+    lng2rad = convertDegreesToRadians(lng2deg); //Convert the longitude of the second city to degrees
+    u = sin((lat2rad - lat1rad)/2.0); //Compute the sine of the latitude differential
+    v = sin((lng2rad - lng1rad)/2.0); //Compute the sine of the longitude diferential
+    
     //Haversine formula
-	return asin(sqrt(pow(sin(latitude/2),2)+pow(sin(longitude/2),2)*cos(latitude1)*cos(latitude2))) * 2 * radius;
-   
+    return 2.0 * EARTH_RADIUS_KM * asin(sqrt(u * u + cos(lat1rad) * cos(lat2rad) * v * v));
 }//end of the computeDistance function
 
 /*struct individual* solution computeOverallDistance(int* population) {
@@ -62,8 +94,11 @@ double computeDistance(int indexCoord1, int indexCoord2, struct city cities[]){
 
 //main function
 int main(){
-    int numberCities = readCitys();//store the number of cities in the file
-    struct city cities[numberCities];//array of structures of city 
-    readInput(cities,numberCities);//read the values of latitude and longitude in the file
+    
+    int numberCities = readCities();//store the number of cities in the file
+    struct city citiesArray[numberCities];//array of structures of city 
+    readInput(citiesArray, numberCities);//read the values of latitude and longitude in the file
+    displayCities(citiesArray, numberCities);
+    
     return 0;
 }//end of the main function
